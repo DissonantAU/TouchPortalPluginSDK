eb628444eeb35a0cba050d30ca4544531650ad3c
DissonantAU <33020221+DissonantAU@users.noreply.github.com>
2025-01-27 18:34:17 +1100

    Packager Plugin - Changes Backported from 9.0.0
    - added UTF encoding to JavaCompile Tasks
    - added dependsOn project.processResources to copyResources

--- Packager/src/main/groovy/com/christophecvb/touchportal/packager/TouchPortalPluginPackager.groovy
+++ Packager/src/main/groovy/com/christophecvb/touchportal/packager/TouchPortalPluginPackager.groovy
@@ -17,8 +17,9 @@ class TouchPortalPluginPackager implements Plugin<Project> {
 
         project.tasks.withType(JavaCompile) { task ->
             task.doFirst {
-                println('Adding -parameters to Compiler Args')
+                println('Adding -parameters to Compiler Args and setting encoding to UTF-8')
                 options.compilerArgs.add('-parameters')
+                options.encoding = "UTF-8"
             }
         }
 
@@ -54,6 +55,7 @@ class TouchPortalPluginPackager implements Plugin<Project> {
 
         def copyResources = project.tasks.register('copyResources', Copy) {
             group = 'Touch Portal Plugin'
+            dependsOn project.processResources
             from(project.file("${project.buildDir}/resources/main/"))
             into("${project.buildDir}/plugin/${extension.mainClassSimpleName.get()}/")
             setDuplicatesStrategy(DuplicatesStrategy.WARN)
